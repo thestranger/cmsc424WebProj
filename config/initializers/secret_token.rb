2f7f289e7ec549a4c0c084a3f090768b81a3d45f
Sawyer Symington <sawsym@hotmail.com>
2012-10-03 21:59:52 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cmsc424WebProj::Application.config.secret_token = 'c76e2e38faf0b3d978bca7860ce7cf54828a3c4b2c30d9fd4ca4ee4aa7309281341bdc5ec915e554dfae2f3550bc1c08de880d9cf02b38fa801143e1b9bfa094'
